460391918801f1d2195aec684ee185e2f5c6989a
atwjsw <wendaniel@hotmail.com>
2017-05-10 19:56:51 -0400

    git init commit

---
+++ src/main/java/com/atwjsw/mmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.atwjsw.mmall.dao;
+
+import com.atwjsw.mmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
